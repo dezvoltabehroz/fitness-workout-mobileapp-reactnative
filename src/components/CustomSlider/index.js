b1d05bc4b34e9e138c7cba67c4e6f966c609dda8
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-11-09 23:28:43 +0500

    hot fixes

--- src/components/CustomSlider/index.js
+++ src/components/CustomSlider/index.js
@@ -34,7 +34,7 @@ export default class CustomSlider extends Component {
                         trackStyle={{ backgroundColor: 'transparent' }}
                         selectedStyle={{ backgroundColor: "transparent" }}
                         values={this.props.single ? [this.state.multiSliderValue[1]] : [this.state.multiSliderValue[0], this.state.multiSliderValue[1]]}
-                        sliderLength={Dimensions.get('window').width * 0.8}
+                        sliderLength={Dimensions.get('window').width * 0.7}
                         onValuesChange={this.multiSliderValuesChange}
                         min={this.props.min}
                         max={this.props.max}
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
     },
     column: {
         flex: 1,
-        width: Dimensions.get('window').width * 0.8,
+        width: Dimensions.get('window').width * 0.7,
         flexDirection: 'row',
         // flexWrap: "wrap",
         alignSelf:"center",
