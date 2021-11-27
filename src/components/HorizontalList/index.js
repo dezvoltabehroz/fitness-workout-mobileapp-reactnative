f9c2cc4c2efcf3016f54d98b7e44ed9b85d3cd15
Mudassar Shahbaz <59355787+mudasarshahbaz@users.noreply.github.com>
2021-11-27 18:52:56 +0500

    hot fixes

--- src/components/HorizontalList/index.js
+++ src/components/HorizontalList/index.js
@@ -109,7 +109,7 @@ const styles = StyleSheet.create({
     },
     overlay: {
         ...StyleSheet.absoluteFillObject,
-        backgroundColor: 'rgba(0,0,0,0.5)',
+        backgroundColor: 'rgba(0,0,0,0.2)',
     },
     // overlay: {
     //     position: 'absolute',
